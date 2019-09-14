273174f4fe2a83a81551ef71edf8e4a34cfbbcde
Hai Nguyen <1512143@student.hcmus.edu.vn>
2019-09-14 23:08:44 +0700

    get activities by type

--- router/v1/activity/index.js
+++ router/v1/activity/index.js
@@ -12,6 +12,15 @@ router.get('/', wrapAsync(async(req, res, next) => {
     return res.status(200).json(activities)
 }))
 
+router.get('/type=:type', wrapAsync(async(req, res, next) => {
+    const { type } = req.params
+    const activities = await Activity.find({type: type},{comment: 0, review: 0, address: 0, images: 0, ranking: 0})
+    if ( activities === null ) {
+        return res.status(404).send({error: 'No Activity Was Found'})
+    }
+    return res.status(200).json(activities)
+}))
+
 router.get('/:id', wrapAsync(async(req, res, next) => {
     const { id } = req.params
     const activityDetail = await Activity.findById(id)
