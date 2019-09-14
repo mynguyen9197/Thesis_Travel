90f97fad3f19e2e90f533a420dfe188176e94b1d
Nguyen Ngoc Xuan My <nnxmy97@gmail.com>
2019-09-14 22:18:21 +0700

    load list activities and detail

--- router/v1/activity/index.js
+++ router/v1/activity/index.js
@@ -3,10 +3,22 @@ const router = express.Router()
 
 const Activity = require(global.appRoot + '/models/activity')
 const { wrapAsync } = require(global.appRoot + '/utils')
+
 router.get('/', wrapAsync(async(req, res, next) => {
-    await Activity.find({}, {_id: 0, comment: 0}).then((result) => {
-        res.status(200).send(result)
-    })
+    const activities = await Activity.find({}, {comment: 0, review: 0, address: 0, images: 0, ranking: 0})
+    if ( activities === null ) {
+        return res.status(404).send({error: 'No Activity Was Found'})
+    }
+    return res.status(200).json(activities)
+}))
+
+router.get('/:id', wrapAsync(async(req, res, next) => {
+    const { id } = req.params
+    const activityDetail = await Activity.findById(id)
+    if ( activityDetail === null ) {
+        return res.status(404).send({error: 'Activity Was Not Found'})
+    }
+    return res.status(200).json(activityDetail)
 }))
 
 module.exports = router
