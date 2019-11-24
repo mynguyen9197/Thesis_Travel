de7e7cfcbe0fa1496716c16130055e9c9d8b2e16
Nguyen Ngoc Xuan My <nnxmy97@gmail.com>
2019-11-24 15:53:28 +0700

    fix add review when null

--- router/v1/user/index.js
+++ router/v1/user/index.js
@@ -119,9 +119,14 @@ router.post("/tour/review", verifyToken, wrapAsync(async(req, res, next) => {
 }))
 
 function calculateRating(review, rating){
-    let ratingList = review ? review[0].review_detail.split(";") : Array(0, 0, 0, 0, 0)
-    ratingList[rating] = parseInt(ratingList[rating].replace(',', '')) + 1
-    ratingList[rating] = ratingList[rating].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    let ratingList = Array(0, 0, 0, 0, 0)
+    if(review[0].review_detail){
+        ratingList = review[0].review_detail.split(";")
+        ratingList[rating] = parseInt(ratingList[rating].replace(',', '')) + 1
+        ratingList[rating] = ratingList[rating].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    } else {
+        ratingList[rating] = 1
+    }
     return ratingList
 }
 
