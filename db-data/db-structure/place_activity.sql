e9733fc5e4cb52ee4b7a236718b40dc85245b387
Nguyen Ngoc Xuan My <nnxmy97@gmail.com>
2020-04-24 11:23:52 +0700

    remove unused data

--- db-data/db-structure/place_activity.sql
+++
@@ -1,6 +0,0 @@
-use hoian_travel;
-
-insert into place_activity(name, category, parent) values ('Theaters', 2, null), ('Performances', 2, null),
-('Gift & Specialty Shops', 3, null), ('Art Galleries', 3, null), ('Shopping Malls', 3, null), ('Flea & Street Markets', 3, null),
-('Shopping Tours', 3, null), ('Factory Outlets', 3, null), ('Department Stores', 3, null), ('Fashion Shows & Tours', 3, null),
-('Taxis & Shuttles', 4, null), ('Mass Transportation Systems', 4, null), ('Bus Transportation', 4, null);
